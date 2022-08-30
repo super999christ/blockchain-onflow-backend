4cfaf919652d7fec1ff96f45a8ec806e1c333bcc
CodyChrist <cr999c999@gmail.com>
2022-08-30 15:56:35 -0700

    Complete test code

---
+++ src/transaction/transaction.resolver.spec.ts
@@ -0,0 +1,194 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TransactionResolver } from './transaction.resolver';
+import { TransactionService } from './transaction.service';
+
+describe('TransactionResolver', () => {
+    let resolver: TransactionResolver;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                TransactionResolver,
+                {
+                    provide: TransactionResolver,
+                    useFactory: () => ({
+                        mint: jest.fn((name: string, description: string, thumbnail: string) => (
+                            {
+                                "transactionId": "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690"
+                            }
+                        )),
+
+                        findMany: jest.fn((address: string) => (
+                            [
+                                {
+                                  id: 0,
+                                  name: 'name1',
+                                  description: 'desc1',
+                                  thumbnail: 'thumb1',
+                                  file: {
+                                    url: 'fake1',
+                                  },
+                                  owner: 'owner1',
+                                },
+                                {
+                                  id: 1,
+                                  name: 'name2',
+                                  description: 'desc2',
+                                  thumbnail: 'thumb2',
+                                  file: {
+                                    url: 'fake2',
+                                  },
+                                  owner: 'owner2',
+                                },
+                                {
+                                  id: 2,
+                                  name: 'name3',
+                                  description: 'desc3',
+                                  thumbnail: 'thumb3',
+                                  file: {
+                                    url: 'fake3',
+                                  },
+                                  owner: 'owner3',
+                                },
+                            ]
+                        )),
+                        findOne: jest.fn((id: Number, address: string) => (
+                            {
+                            id: 0,
+                            name: 'name1',
+                            description: 'desc1',
+                            thumbnail: 'thumb1',
+                            file: {
+                              url: 'fake1',
+                            },
+                            owner: 'owner1',
+                            }
+                        )),
+                        burn: jest.fn((id: number) => (
+                            {
+                                "transactionId": "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690"
+                            }
+                        )),
+                        transfer: jest.fn((id: number, receiver: string) => (
+                            {
+                                "transactionId": "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690"
+                            }
+                        ))
+                    })
+                }
+            ],
+        }).compile();
+        resolver = module.get<TransactionResolver>(TransactionResolver);
+    });
+
+    it("should be defined", () => {
+        expect(resolver).toBeDefined();
+    });
+
+    describe('mint', () => {
+        // eslint-disable-next-line prettier/prettier
+        it('should create an ExampleNFT on the blockchain ', () => {
+            expect(
+                resolver.mint(
+                    'A sample name.',
+                    'A sample description.',
+                    'A sample thumbnail.',
+                )
+            ).toEqual(
+                'e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690',
+            );
+        });
+    });
+
+    describe('findMany', () => {
+        it('should take an address as input and return all the NFTs owned by the address', () => {
+            expect(
+                resolver.findMany(
+                    "0x01cf0e2f2f715450"
+                )
+            ).toEqual(
+                [
+                    {
+                      id: 0,
+                      name: 'name1',
+                      description: 'desc1',
+                      thumbnail: 'thumb1',
+                      file: {
+                        url: 'fake1',
+                      },
+                      owner: 'owner1',
+                    },
+                    {
+                      id: 1,
+                      name: 'name2',
+                      description: 'desc2',
+                      thumbnail: 'thumb2',
+                      file: {
+                        url: 'fake2',
+                      },
+                      owner: 'owner2',
+                    },
+                    {
+                      id: 2,
+                      name: 'name3',
+                      description: 'desc3',
+                      thumbnail: 'thumb3',
+                      file: {
+                        url: 'fake3',
+                      },
+                      owner: 'owner3',
+                    },
+                ]
+            );
+        })
+    })
+
+    describe('findOne', () => {
+        it("should take an address and NFT ID as input", () => {
+            expect(
+                resolver.findOne(
+                    0,
+                    "0x01cf0e2f2f715450"
+                )
+            ).toEqual(
+                {
+                    id: 0,
+                    name: 'name1',
+                    description: 'desc1',
+                    thumbnail: 'thumb1',
+                    file: {
+                      url: 'fake1',
+                    },
+                    owner: 'owner1',
+                }
+            )
+        })
+    })
+
+    describe('burn', () => {
+        it("should take an NFT ID as input and delete the ExampleNFT with the corresponding ID from our development account", () => {
+            expect(
+                resolver.burn(
+                    0
+                )
+            ).toEqual(
+                "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690"
+            )
+        })
+    })
+
+    describe('transfer', () => {
+        it("should transfer a specific ExampleNFT from our development account to the specified address", () => {
+            expect(
+                resolver.transfer(
+                    // The ID of the NFT to transfer
+                    0,
+                    // The address of the account that will receive the NFT
+                    "0x01cf0e2f2f715450"
+                )
+            ).toEqual(
+                "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690"
+            )
+        })
+    })
+});
---
+++ src/transaction/transaction.service.spec.ts
@@ -0,0 +1,185 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TransactionService } from './transaction.service';
+import { BadRequestException } from '@nestjs/common';
+import { Data } from './data.interface';
+
+
+
+
+
+const setData = (fakedata) => {
+
+}
+
+
+const mockData = (
+  id,
+  name,
+  description,
+  thumbnail
+): Data => ({
+  id,
+  name,
+  description,
+  thumbnail
+});
+
+const getMetaData = () => {
+  return 
+}
+
+
+describe('TransactionService', () => {
+  // eslint-disable-next-line prettier/prettier
+  let service: TransactionService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TransactionService],
+    }).compile();
+    service = module.get<TransactionService>(TransactionService);
+
+    const fakeData = [
+        {
+          id: 0,
+          name: 'name1',
+          description: 'desc1',
+          thumbnail: 'thumb1',
+          file: {
+            url: 'fake1',
+          },
+          owner: 'owner1',
+        },
+        {
+          id: 1,
+          name: 'name2',
+          description: 'desc2',
+          thumbnail: 'thumb2',
+          file: {
+            url: 'fake2',
+          },
+          owner: 'owner2',
+        },
+        {
+          id: 2,
+          name: 'name3',
+          description: 'desc3',
+          thumbnail: 'thumb3',
+          file: {
+            url: 'fake3',
+          },
+          owner: 'owner3',
+        },
+      ];
+    setData(fakeData);
+
+
+    // addData
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('mint', () => {
+    // eslint-disable-next-line prettier/prettier
+    it('should create an ExampleNFT on the blockchain ', () => {
+      // expect(
+      //   service.mint(
+      //     'A sample name.',
+      //     'A sample description.',
+      //     'A sample thumbnail.',
+      //   )
+      // ).toEqual(
+      //   'e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690'
+      // );
+      expect( () => {
+        let result= service.mint(
+          'A sample name.',
+          'A sample description.',
+          'A sample thumbnail.',
+        )
+        let regPattern = /[0-9a-f]{64}/g;
+        expect(regPattern.test(result)).toBe(true);
+      }
+      )
+    });
+  });
+
+  describe('findMany', () => {
+    it('should take an address as input and return all the NFTs owned by the address', () => {
+      expect(service.findMany('0x01cf0e2f2f715450')).toEqual([
+        {
+          id: 0,
+          name: 'name1',
+          description: 'desc1',
+          thumbnail: 'thumb1',
+        },
+        {
+          id: 1,
+          name: 'name2',
+          description: 'desc2',
+          thumbnail: 'thumb2',
+        }
+      ]);
+    }); 
+  });
+
+  describe('findOne', () => {
+    it('should take an address and NFT ID as input', () => {
+      expect(service.findOne(0, '0x01cf0e2f2f715450')).toEqual({
+        data: {
+          name: 'name1',
+          description: 'desc1',
+          thumbnail: 'thumb1',
+        },
+      });
+    });
+  });
+
+  describe('burn', () => {
+    it('should take an NFT ID as input and delete the ExampleNFT with the corresponding ID from our development account', () => {
+      expect( () => {
+        let result= service.burn(0);
+        let regPattern = /[0-9a-f]{64}/g;
+        expect(regPattern.test(result)).toBe(true);
+        const fakeData1 = [
+          {
+            id: 1,
+            name: 'name2',
+            description: 'desc2',
+            thumbnail: 'thumb2',
+            file: {
+              url: 'fake2',
+            },
+            owner: 'owner2',
+          },
+          {
+            id: 2,
+            name: 'name3',
+            description: 'desc3',
+            thumbnail: 'thumb3',
+            file: {
+              url: 'fake3',
+            },
+            owner: 'owner3',
+          }
+        ]
+        const metaData = getMetaData();
+        expect(metaData).toEqual(fakeData1)
+      }
+      )
+    });
+  });
+
+  describe('transfer', () => {
+    it('should transfer a specific ExampleNFT from our development account to the specified address', () => {
+      expect( () => {
+        let result= service.transfer(0, "0x01cf0e2f2f715450")
+        let regPattern = /[0-9a-f]{64}/g;
+        expect(regPattern.test(result)).toBe(true);
+      }
+      )
+    });
+  });
+});
