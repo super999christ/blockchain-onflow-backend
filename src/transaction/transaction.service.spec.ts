d66bf206393d26d4dece69497f4912241486c3d1
CodyChrist <cr999c999@gmail.com>
2022-08-31 04:25:18 -0700

    Check the FindOne, Burn, Mint

--- src/transaction/transaction.service.spec.ts
+++ src/transaction/transaction.service.spec.ts
@@ -1,35 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TransactionService } from './transaction.service';
-import { BadRequestException } from '@nestjs/common';
-import { Data } from './data.interface';
-
-
-
-
+import { Test, TestingModule } from "@nestjs/testing";
+import { TransactionService } from "./transaction.service";
+import { BadRequestException } from "@nestjs/common";
+import { Data } from "./data.interface";
 
 const setData = (fakedata) => {
 
-}
+};
 
-
-const mockData = (
+const mockData = (id, name, description, thumbnail): Data => ({
   id,
   name,
   description,
-  thumbnail
-): Data => ({
-  id,
-  name,
-  description,
-  thumbnail
+  thumbnail,
 });
 
 const getMetaData = () => {
-  return 
-}
+  return;
+};
 
-
-describe('TransactionService', () => {
+describe("TransactionService", () => {
   // eslint-disable-next-line prettier/prettier
   let service: TransactionService;
 
@@ -40,48 +29,35 @@ describe('TransactionService', () => {
     service = module.get<TransactionService>(TransactionService);
 
     const fakeData = [
-        {
-          id: 0,
-          name: 'name1',
-          description: 'desc1',
-          thumbnail: 'thumb1',
-          file: {
-            url: 'fake1',
-          },
-          owner: 'owner1',
-        },
-        {
-          id: 1,
-          name: 'name2',
-          description: 'desc2',
-          thumbnail: 'thumb2',
-          file: {
-            url: 'fake2',
-          },
-          owner: 'owner2',
-        },
-        {
-          id: 2,
-          name: 'name3',
-          description: 'desc3',
-          thumbnail: 'thumb3',
-          file: {
-            url: 'fake3',
-          },
-          owner: 'owner3',
-        },
-      ];
+      {
+        id: 0,
+        name: "name1",
+        description: "desc1",
+        thumbnail: "thumb1",
+      },
+      {
+        id: 1,
+        name: "name2",
+        description: "desc2",
+        thumbnail: "thumb2",
+      },
+      {
+        id: 2,
+        name: "name3",
+        description: "desc3",
+        thumbnail: "thumb3",
+      },
+    ];
     setData(fakeData);
-
 
     // addData
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 
-  describe('mint', () => {
+  describe("mint", () => {
     // eslint-disable-next-line prettier/prettier
     it('should create an ExampleNFT on the blockchain ', () => {
       // expect(
@@ -93,93 +69,90 @@ describe('TransactionService', () => {
       // ).toEqual(
       //   'e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690'
       // );
-      expect( () => {
-        let result= service.mint(
-          'A sample name.',
-          'A sample description.',
-          'A sample thumbnail.',
-        )
-        let regPattern = /[0-9a-f]{64}/g;
+      expect(() => {
+        const result = service.mint(
+          "A sample name.",
+          "A sample description.",
+          "A sample thumbnail."
+        );
+        const regPattern = /[0-9a-f]{64}/g;
         expect(regPattern.test(result)).toBe(true);
-      }
-      )
+      });
     });
   });
 
-  describe('findMany', () => {
-    it('should take an address as input and return all the NFTs owned by the address', () => {
-      expect(service.findMany('0x01cf0e2f2f715450')).toEqual([
+  describe("findMany", () => {
+    it("should take an address as input and return all the NFTs owned by the address", () => {
+      expect(service.findMany("0x01cf0e2f2f715450")).toEqual([
         {
           id: 0,
-          name: 'name1',
-          description: 'desc1',
-          thumbnail: 'thumb1',
+          name: "name1",
+          description: "desc1",
+          thumbnail: "thumb1",
         },
         {
           id: 1,
-          name: 'name2',
-          description: 'desc2',
-          thumbnail: 'thumb2',
-        }
+          name: "name2",
+          description: "desc2",
+          thumbnail: "thumb2",
+        },
       ]);
-    }); 
+    });
   });
 
-  describe('findOne', () => {
-    it('should take an address and NFT ID as input', () => {
-      expect(service.findOne(0, '0x01cf0e2f2f715450')).toEqual({
+  describe("findOne", () => {
+    it("should take an address and NFT ID as input", () => {
+      expect(service.findOne(0, "0x01cf0e2f2f715450")).toEqual({
         data: {
-          name: 'name1',
-          description: 'desc1',
-          thumbnail: 'thumb1',
+          name: "name1",
+          description: "desc1",
+          thumbnail: "thumb1",
         },
       });
     });
   });
 
-  describe('burn', () => {
-    it('should take an NFT ID as input and delete the ExampleNFT with the corresponding ID from our development account', () => {
-      expect( () => {
-        let result= service.burn(0);
-        let regPattern = /[0-9a-f]{64}/g;
+  describe("burn", () => {
+    it("should take an NFT ID as input and delete the ExampleNFT with the corresponding ID from our development account", () => {
+      expect(() => {
+        const result = service.burn(0);
+        const regPattern = /[0-9a-f]{64}/g;
         expect(regPattern.test(result)).toBe(true);
         const fakeData1 = [
           {
             id: 1,
-            name: 'name2',
-            description: 'desc2',
-            thumbnail: 'thumb2',
+            name: "name2",
+            description: "desc2",
+            thumbnail: "thumb2",
             file: {
-              url: 'fake2',
+              url: "fake2",
             },
-            owner: 'owner2',
+            owner: "owner2",
           },
           {
             id: 2,
-            name: 'name3',
-            description: 'desc3',
-            thumbnail: 'thumb3',
+            name: "name3",
+            description: "desc3",
+            thumbnail: "thumb3",
             file: {
-              url: 'fake3',
+              url: "fake3",
             },
-            owner: 'owner3',
-          }
-        ]
+            owner: "owner3",
+          },
+        ];
         const metaData = getMetaData();
-        expect(metaData).toEqual(fakeData1)
-      }
-      )
+        expect(metaData).toEqual(fakeData1);
+      });
     });
   });
 
-  describe('transfer', () => {
-    it('should transfer a specific ExampleNFT from our development account to the specified address', () => {
-      expect( () => {
-        let result= service.transfer(0, "0x01cf0e2f2f715450")
-        let regPattern = /[0-9a-f]{64}/g;
+  describe("transfer", () => {
+    it("should transfer a specific ExampleNFT from our development account to the specified address", () => {
+      expect(() => {
+        const result = service.transfer(0, "0x01cf0e2f2f715450");
+        const regPattern = /[0-9a-f]{64}/g;
         expect(regPattern.test(result)).toBe(true);
-      }
-      )
+      });
     });
   });
 });
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,11 +1,11 @@
 //  External Dependencies
-import { Injectable } from '@nestjs/common';
-import * as fcl from '@onflow/fcl';
+import { Injectable } from "@nestjs/common";
+import * as fcl from "@onflow/fcl";
 
 //  Internal Dependencies
-import { NFT } from './types/nft.types';
-import { authorizationFunction } from '../flow-auth';
-import { mintTx } from '../../cadence/transactions/apis';
+import { NFT } from "./types/nft.types";
+import { authorizationFunction } from "../flow-auth";
+import { mintTx } from "../../cadence/transactions/apis";
 
 @Injectable()
 export class TransactionService {
@@ -13,14 +13,14 @@ export class TransactionService {
 
   //  Returns Test String
   test(): string {
-    return 'GraphQL: Transaction works';
+    return "GraphQL: Transaction works";
   }
 
   //  Create the NFT on the blockchain and return the transactionID
   async mint(
     name: string,
     description: string,
-    thumbnail: string,
+    thumbnail: string
   ): Promise<string> {
     const transactionId = await fcl.mutate({
       cadence: mintTx,
@@ -48,33 +48,33 @@ export class TransactionService {
     return [
       {
         id: 0,
-        name: 'name1',
-        description: 'desc1',
-        thumbnail: 'thumb1',
+        name: "name1",
+        description: "desc1",
+        thumbnail: "thumb1",
         file: {
-          url: 'fake1',
+          url: "fake1",
         },
-        owner: 'owner1',
+        owner: "owner1",
       },
       {
         id: 1,
-        name: 'name2',
-        description: 'desc2',
-        thumbnail: 'thumb2',
+        name: "name2",
+        description: "desc2",
+        thumbnail: "thumb2",
         file: {
-          url: 'fake2',
+          url: "fake2",
         },
-        owner: 'owner2',
+        owner: "owner2",
       },
       {
         id: 2,
-        name: 'name3',
-        description: 'desc3',
-        thumbnail: 'thumb3',
+        name: "name3",
+        description: "desc3",
+        thumbnail: "thumb3",
         file: {
-          url: 'fake3',
+          url: "fake3",
         },
-        owner: 'owner3',
+        owner: "owner3",
       },
     ];
   }
@@ -85,13 +85,13 @@ export class TransactionService {
 
     return {
       id: 0,
-      name: 'name1',
-      description: 'desc1',
-      thumbnail: 'thumb1',
+      name: "name1",
+      description: "desc1",
+      thumbnail: "thumb1",
       file: {
-        url: 'fake1',
+        url: "fake1",
       },
-      owner: 'owner1',
+      owner: "owner1",
     };
   }
 
@@ -99,13 +99,13 @@ export class TransactionService {
   burn(id: number): string {
     //  TODO: Find the NFT data
     //  TODO: Delete it from blockchain
-    return 'e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690';
+    return "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690";
   }
 
   //  Trnasfer NFT with id to receiver address
   transfer(id: number, receiver: string): string {
     //  TODO: Get NFT data and change owner
 
-    return 'e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690';
+    return "e0b97916b821adc178390f075bb44bb6e42d0617ec2c51bf966f40506e79d690";
   }
 }
